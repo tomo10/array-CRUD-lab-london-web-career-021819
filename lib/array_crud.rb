158caf1df67a1d58b1ea75aec3ae50d3a6ddf7d9
Thomas Edwards <thce28@gmail.com>
2019-02-07 16:38:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["Alpha", "Bravo", "Charlie", "Delta"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.
 end
 
 def remove_element_from_end_of_array(array)
